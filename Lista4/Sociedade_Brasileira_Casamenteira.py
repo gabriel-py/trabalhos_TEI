d66f201cea4bbb45b45461b57b30db3de2979111
Joao-Queiroga <72106566+Joao-Queiroga@users.noreply.github.com>
2021-09-12 15:26:49 -0300

    Update Sociedade_Brasileira_Casamenteira.py
    Time Limit Exceeded

--- Lista4/Sociedade_Brasileira_Casamenteira.py
+++ Lista4/Sociedade_Brasileira_Casamenteira.py
@@ -1,60 +1,44 @@
 #João Francisco Carvalho Soares de Oliveira Queiroga - 20192020135
 #Gabriel Gonçalves de Souza Ribeiro - 20192003573
 #Raquel Alexsandra do Couto - 201811130372
-#A solução implementada transforma os pares de pessoas apaixonadas em um grafo ordenado em que cada pessoa é uma aresta
-#e está ligada ao vértice em que esta pessoa está apaixonada.
-#é feita então uma DFS para ver se é completado um ciclo no grafo ordenado da pesssoa até ela mesma
+#A solução implementada usa dicionarios para armazenar o grafo em que cada pessoa é um nó e é ligado a outra..
+#É feita então uma DFS para ver se é completado um ciclo no grafo ordenado da pesssoa até ela mesma
 #cada ciclo completo é um casal formado.
 
-def dfs(vertice, caminho = []):
-    caminho.append(vertice)
-    for vizinho in grafo[vertice]:
-        if caminho[len(caminho) - 1] == caminho[0]:
+def dfs(vertice_inicio):
+    caminho = [vertice_inicio]
+    falta_visitar = [vertice_inicio]
+    while falta_visitar:
+        vertice = falta_visitar.pop()
+        if len(caminho) > 1 and vertice == caminho[0]:
             return caminho
-        if vizinho not in caminho or vizinho == caminho[0]:
-            dfs(vizinho, caminho)
-        return caminho
+        if grafo[vertice] not in caminho or grafo[vertice] == caminho[0]:
+            caminho.append(grafo[vertice])
+            falta_visitar.append(grafo[vertice])
+    return caminho
 
 def tem_ciclo(vertice):
     busca = dfs(vertice)
     if busca[0] == busca[len(busca)-1]:
+        for vertice in busca:
+            verificado[vertice] = True
         return True
     else:
         return False
 
-def cria_adjacencia(par):
-    for i in range(n_pessoas):
-        if pessoas[i] == par[0]:
-            for j in range(n_pessoas):
-                if pessoas[j] == par[1]:
-                    grafo[i].append(j)
-                    return
-paixoes = []
+grafo = dict()
+verificado = dict()
 while True:
     try:
-        paixoes.append(input().split())
+        nome1, nome2 = map(str, input().split())
+        grafo[nome1] = nome2
+        verificado[nome1] = False
     except EOFError:
         break
 
-pessoas = []
-
-for par in paixoes:
-    for pessoa in par:
-        if pessoa not in pessoas:
-            pessoas.append(pessoa)
-
-n_pessoas = len(pessoas)
-
-grafo = [[] for j in range(n_pessoas)]
-verificado = [0 for i in range(n_pessoas)]
-for par in paixoes:
-    cria_adjacencia(par)
-
 n_casais = 0
 
-print(grafo)
-
-for i in range(n_pessoas):
-    if verificado[i] == 0 and tem_ciclo(i):
+for vertice in grafo:
+    if verificado[vertice] == False and tem_ciclo(vertice):
         n_casais += 1
 print(n_casais)
