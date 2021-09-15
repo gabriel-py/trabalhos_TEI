3da51dd84bd1ba4e2550d02dca00b6b4ac79dc47
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2021-09-15 20:24:50 -0300

    Changes to be committed: 	modified:   Viagem_para_BH.py  Untracked files: 	../.idea/ 	entrada 	viagem_cola.py

--- Trabalho2/Viagem_para_BH.py
+++ Trabalho2/Viagem_para_BH.py
@@ -1,30 +1,30 @@
 #Ainda não está pronto
-def menor_preco(array):
-    menor = array[0][1]
-    for i in range(len(array)):
-        if array[i][1] < menor:
-            menor = array[i][1]
-    return menor
+from collections import defaultdict
+import heapq
 
+class Viagem:
 
-def dfs(vertice_fonte):
-    nao_resolvido = [vertice_fonte]
-    while nao_resolvido:
-        vertice = nao_resolvido.pop(menor_preco(nao_resolvido))
+    def __init__(self):
+        self.grafo = defaultdict(list)
+
+    def add_aresta(self, A, B, R):
+        self.grafo[A].append((B, R))
+
+    def menor_caminho(self):
+        pass
+
 
 N, M = map(int, input().split())
 
-grafo_onibus = [[] for i in range(N)]
-grafo_aviao = [[] for i in range(N)]
-
+viagem_onibus = Viagem()
+viagem_aviao = Viagem()
+print(N,M)
 while True:
     try:
         A, B, T, R = map(int, input().split())
         if T == 1:
-            grafo_aviao[A - 1].append([B, R])
-            pass
+            viagem_aviao.add_aresta(A, B, R)
         else:
-            grafo_onibus[A - 1].append([B, R])
+            viagem_onibus.add_aresta(A, B, R)
     except EOFError:
         break
-print(grafo_onibus)
