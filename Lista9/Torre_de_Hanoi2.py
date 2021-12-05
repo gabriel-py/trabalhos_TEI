d7718712d8d701e0bed527610ae2046f1fdecc42
João Queiroga <joaoqueiroga@arch.localdomain>
2021-12-05 16:24:00 -0300

    atualizada torre de hanoi 2

--- Lista9/Torre_de_Hanoi2.py
+++ Lista9/Torre_de_Hanoi2.py
@@ -1,7 +1,7 @@
 #
 def eh_quadrado(num):
     raiz = int(num ** 1/2)
-    if raiz ** 2 == num:
+    if (raiz ** 2) == num:
         return True
     else:
         return False
@@ -11,3 +11,19 @@ T = int(input())
 for i in range(T):
     N = int(input())
     n_bola = 1
+    arestas = [[] for j in range(N)]
+    while True:
+        n_bola+=1
+        colocado = False
+        for aresta in arestas:
+            if len(aresta) == 0 and not colocado:
+                aresta.append(n_bola)
+                colocado = True
+                break
+            elif eh_quadrado(aresta[-1] + n_bola) and not colocado:
+                aresta.append(n_bola)
+                colocado = True
+                break
+        if not colocado:
+            break
+    print(n_bola)
