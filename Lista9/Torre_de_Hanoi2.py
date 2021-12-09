094d148b0227491aec7eabeae0b92dc3a2637e70
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2021-12-09 09:14:36 -0300

    resolvida torre de hanoi 2

--- Lista9/Torre_de_Hanoi2.py
+++ Lista9/Torre_de_Hanoi2.py
@@ -1,11 +1,15 @@
-#
+#João Francisco Carvalho Soares de Oliveira Queiroga - 20192020135
+#Gabriel Gonçalves de Souza Ribeiro - 20192003573
+#Raquel Alexsandra do Couto - 201811130372
+# A solucao implementada verifica, no momento de colocar a bola, se a soma da numero da bola a ser colocada com o 
+#número da última bola da vareta forma um número quadrado. O numero de bolas colocadas é o numero da ultima bola - 1
 def eh_quadrado(num):
-    raiz = int(num ** 1/2)
+    raiz = int(num ** (1/2))
     if (raiz ** 2) == num:
         return True
     else:
         return False
-
+    
 T = int(input())
 
 for i in range(T):
@@ -13,7 +17,6 @@ for i in range(T):
     n_bola = 1
     arestas = [[] for j in range(N)]
     while True:
-        n_bola+=1
         colocado = False
         for aresta in arestas:
             if len(aresta) == 0 and not colocado:
@@ -26,4 +29,5 @@ for i in range(T):
                 break
         if not colocado:
             break
-    print(n_bola)
+        n_bola+=1
+    print(n_bola - 1)
